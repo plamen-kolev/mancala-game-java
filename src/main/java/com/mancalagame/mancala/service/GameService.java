cc1210b99d6c1b2b07c5dda04672b7671f65f4fb
Plamen Kolev <p.kolev22@gmail.com>
2020-03-31 15:16:23 +0300

    added test for getting an extra turn

--- src/main/java/com/mancalagame/mancala/service/GameService.java
+++ src/main/java/com/mancalagame/mancala/service/GameService.java
@@ -1,5 +1,6 @@
 package com.mancalagame.mancala.service;
 
+import com.mancalagame.mancala.enums.GameState;
 import com.mancalagame.mancala.exceptions.IllegalPlayerMoveException;
 import com.mancalagame.mancala.enums.Player;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +10,28 @@ import org.springframework.stereotype.Service;
 public class GameService {
     private PlayerTurnService turnService;
     private BoardService boardService;
+    private boolean gameWon;
 
     @Autowired
     public GameService(PlayerTurnService turnService, BoardService boardService){
         this.turnService = turnService;
         this.boardService = boardService;
+        this.gameWon = false;
     }
 
-    public synchronized void play(int pitId) throws IllegalPlayerMoveException {
+    public synchronized GameState play(int pitId) throws IllegalPlayerMoveException {
+        if (gameWon) {
+            return GameState.GAME_WON;
+        }
+
         Player currentPlayer = turnService.getCurrentPlayer();
-        this.boardService.play(pitId, currentPlayer);
-        this.turnService.changeTurn();
+        GameState state = this.boardService.play(pitId, currentPlayer);
+        if(!GameState.EXTRA_TURN.equals(state)) {
+            this.turnService.changeTurn();
+        }
+        if(GameState.GAME_WON.equals(state)) {
+            gameWon = true;
+        }
+        return state;
     }
 }
--- src/test/java/com/mancalagame/mancala/service/GameServiceTest.java
+++ src/test/java/com/mancalagame/mancala/service/GameServiceTest.java
@@ -1,11 +1,12 @@
 package com.mancalagame.mancala.service;
 
+import com.mancalagame.mancala.enums.GameState;
 import com.mancalagame.mancala.exceptions.IllegalPlayerMoveException;
 import com.mancalagame.mancala.enums.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.*;
 
 class GameServiceTest {
@@ -40,7 +41,18 @@ class GameServiceTest {
     }
 
     @Test
-    public void whenLastStoneInBigPit_playerGetsAnotherRound() {
-        assertTrue(false);
+    public void dontLoseTurnWhenBoardServiceGivesYouExtraRound() throws IllegalPlayerMoveException {
+        int somePitId = 5;
+        GameState expectedState = GameState.EXTRA_TURN;
+        when(turnService.getCurrentPlayer()).thenReturn(CURRENT_PLAYER);
+        when(boardService.play(somePitId, CURRENT_PLAYER)).thenReturn(expectedState);
+
+        // when
+        GameState nextState = gameService.play(somePitId);
+
+        //then
+        assertThat(nextState, is(expectedState));
+        verify(turnService, times(0)).changeTurn();
+
     }
 }
